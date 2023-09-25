2395f466bd1dfe4ca3d530baea8f67d2c27ef2a8
ZAHEERR-XD <143983806+ZAHEERR-XD@users.noreply.github.com>
2023-09-25 12:40:00 +0500

    Update SALLAH.py

--- SALLAH.py
+++ SALLAH.py
@@ -13,7 +13,7 @@ except ModuleNotFoundError:
     os.system('pip install requests bs4 futures==2 > /dev/null')
     os.system('python SALLAH.py')
 	
-os.system('xdg-open https://www.facebook.com/zafarjaan.baloch.7923 
+os.system('xdg-open https://www.facebook.com/zafarjaan.baloch.7923`) 
 
 try:
     prox= requests.get('https://raw.githubusercontent.com/trt-Fire/data/main/proxies.txt').text
